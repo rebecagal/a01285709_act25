c035a2792568a21dc7b23a0070fe03afd96037cb
Rebeca Galván <70342468+rebecagal@users.noreply.github.com>
2020-10-15 23:16:46 -0500

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,45 @@
+input.onPinPressed(TouchPin.P0, function () {
+    x = 4
+    y = 0
+    while (y <= 4) {
+        led.plot(x, y)
+        x += -1
+        y += 1
+        basic.pause(100)
+    }
+    basic.clearScreen()
+})
+input.onButtonPressed(Button.A, function () {
+    counter = 5
+    while (counter >= 0) {
+        basic.showNumber(counter)
+        counter += -1
+        basic.pause(200)
+    }
+    music.playMelody("C G F D B E A C5 ", 120)
+    basic.clearScreen()
+})
+input.onPinPressed(TouchPin.P1, function () {
+    while (input.lightLevel() < 180) {
+        basic.showIcon(IconNames.Sad)
+        music.playTone(147, music.beat(BeatFraction.Double))
+    }
+    while (input.lightLevel() >= 180) {
+        basic.showIcon(IconNames.Happy)
+        music.playTone(294, music.beat(BeatFraction.Double))
+    }
+    basic.clearScreen()
+})
+let counter = 0
+let y = 0
+let x = 0
+let sprite = game.createSprite(1, 3)
+basic.forever(function () {
+    while (input.buttonIsPressed(Button.B)) {
+        sprite.move(2)
+        basic.pause(100)
+        sprite.turn(Direction.Left, 90)
+        basic.pause(100)
+    }
+    basic.clearScreen()
+})
